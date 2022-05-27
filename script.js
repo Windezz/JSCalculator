f114bb3b189a6a1e7fa01b60a98eb9e0972dd279
Windezz <123edwinthung@gmail.com>
2022-05-27 09:34:45 +0800

    add a function to create buttons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const buttonDiv = document.querySelector('.CButtons');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    for (let i = 0; i < 10; i++) {
+        let newButton = document.createElement('button');
+        newButton.textContent = i;
+        buttonDiv.appendChild(newButton).className = `button${i}`;
+    }
+})
+
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +25,10 @@ function multiply(a, b) {
 function divide (a, b ) {
     return a / b;
 }
+
+function operate(operator, number1, number2) {
+    let output = 0;
+    output = operator(number1, number2);
+    return output;
+}
+
